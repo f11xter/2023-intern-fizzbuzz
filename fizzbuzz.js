058e85c51be7e461037808e68be061d61c4f3863
Felix Waller <felix.waller@softwire.com>
2023-06-26 17:23:47 +0100

    Complete part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,11 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
-    // Put your code here...
-
+    for (let i = 1; i <= 100; i++) {
+        let out = "";
+        if (i % 3 === 0) out += "Fizz";
+        if (i % 5 === 0) out += "Buzz";
+        console.log(out === "" ? i : out);
+    }
 }
 
 // Now, we run the main function:
