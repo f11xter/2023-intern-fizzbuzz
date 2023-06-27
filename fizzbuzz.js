bda4387bf2f5c07352530aa2ef633737bbbe69a7
Felix Waller <felix.waller@softwire.com>
2023-06-27 11:25:16 +0100

    Refactor `fizzbuzz` to enable testing

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,33 +2,68 @@ const prompt = require("prompt-sync")();
 
 // This is our main function
 function fizzbuzz() {
-    let rules = [];
+    const max = getMax();
+    const rules = parseActiveRules();
+
+    for (let i = 1; i <= max; i++) {
+        console.log(getFizzBuzzValue(i, rules));
+    }
+}
+
+/**
+ * Gets max number from user input
+ *
+ * Keeps trying till positive number entered
+ * @returns {number} max number
+ */
+function getMax() {
+    let max = NaN;
+
+    while (isNaN(max) || max <= 0) {
+        max = prompt("Enter maximum number: ");
+    }
+
+    return max;
+}
+
+/**
+ * Parses command line arguments for rule numbers to activate
+ * @returns {number[]} array of active rule numbers
+ */
+function parseActiveRules() {
+    const rules = [];
+
     for (const input of process.argv) {
         if (!isNaN(input) && input > 0) {
             rules.push(Number.parseInt(input));
         }
     }
 
-    let max = NaN;
-    while (isNaN(max) || max <= 0) {
-        max = prompt("Enter maximum number: ");
+    return rules;
+}
+
+/**
+ * Get value of number given active FizzBuzz rules
+ * @param i {number} input number
+ * @param rules {number[]} active rules
+ * @returns {string}
+ */
+function getFizzBuzzValue(i, rules) {
+    let out = [];
+
+    if (rules.includes(3) && i % 3 === 0) out.push("Fizz");
+    if (rules.includes(13) && i % 13 === 0) out.push("Fezz");
+    if (rules.includes(5) && i % 5 === 0) out.push("Buzz");
+    if (rules.includes(7) && i % 7 === 0) out.push("Bang");
+    if (rules.includes(11) && i % 11 === 0) {
+        if (rules.includes(13) && i % 13 === 0) out = ["Fezz", "Bong"];
+        else out = ["Bong"];
     }
+    if (rules.includes(17) && i % 17 === 0) out.reverse();
 
-    for (let i = 1; i <= max; i++) {
-        let out = [];
+    if (out.length === 0) out = [i];
 
-        if (rules.includes(3) && i % 3 === 0) out.push("Fizz");
-        if (rules.includes(13) && i % 13 === 0) out.push("Fezz");
-        if (rules.includes(5) && i % 5 === 0) out.push("Buzz");
-        if (rules.includes(7) && i % 7 === 0) out.push("Bang");
-        if (rules.includes(11) && i % 11 === 0) {
-            if (rules.includes(13) && i % 13 === 0) out = ["Fezz", "Bong"];
-            else out = ["Bong"];
-        }
-        if (rules.includes(17) && i % 17 === 0) out.reverse();
-
-        console.log(out.length === 0 ? i : out.join(''));
-    }
+    return out.join("");
 }
 
 function fizzbuzzcustom(max, rules) {
