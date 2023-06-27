977ca02f4195a38f4d86d22ad01f72d069466cf8
Felix Waller <felix.waller@softwire.com>
2023-06-27 09:37:23 +0100

    Add rules for 7, 11, 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,13 @@ function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
         let out = "";
         if (i % 3 === 0) out += "Fizz";
+        if (i % 13 === 0) out += "Fezz";
         if (i % 5 === 0) out += "Buzz";
+        if (i % 7 === 0) out += "Bang";
+        if (i % 11 === 0) {
+            out = "Bong";
+            if (i % 13 === 0) out = "Fezz" + out;
+        }
         console.log(out === "" ? i : out);
     }
 }
