d3696b396c480c42b508aeae64156fdcf4812f0e
Felix Waller <felix.waller@softwire.com>
2023-06-27 09:58:51 +0100

    Add user input validation

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,14 @@ const prompt = require("prompt-sync")();
 
 // This is our main function
 function fizzbuzz() {
-    const max = prompt("Enter number: ");
+    let max = NaN;
+    while (isNaN(max) || max <= 0) {
+        max = prompt("Enter maximum number: ");
+    }
 
     for (let i = 1; i <= max; i++) {
         let out = [];
+
         if (i % 3 === 0) out.push("Fizz");
         if (i % 13 === 0) out.push("Fezz");
         if (i % 5 === 0) out.push("Buzz");
@@ -15,6 +19,7 @@ function fizzbuzz() {
             else out = ["Bong"];
         }
         if (i % 17 === 0) out.reverse();
+
         console.log(out.length === 0 ? i : out.join(''));
     }
 }
