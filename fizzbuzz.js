7266ad1e2631c95735e523a9ae02f2ba41e07f89
Felix Waller <felix.waller@softwire.com>
2023-06-27 11:08:43 +0100

    Add command line args to specify active rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,6 +2,13 @@ const prompt = require("prompt-sync")();
 
 // This is our main function
 function fizzbuzz() {
+    let rules = [];
+    for (const input of process.argv) {
+        if (!isNaN(input) && input > 0) {
+            rules.push(Number.parseInt(input));
+        }
+    }
+
     let max = NaN;
     while (isNaN(max) || max <= 0) {
         max = prompt("Enter maximum number: ");
@@ -10,15 +17,15 @@ function fizzbuzz() {
     for (let i = 1; i <= max; i++) {
         let out = [];
 
-        if (i % 3 === 0) out.push("Fizz");
-        if (i % 13 === 0) out.push("Fezz");
-        if (i % 5 === 0) out.push("Buzz");
-        if (i % 7 === 0) out.push("Bang");
-        if (i % 11 === 0) {
-            if (i % 13 === 0) out = ["Fezz", "Bong"];
+        if (rules.includes(3) && i % 3 === 0) out.push("Fizz");
+        if (rules.includes(13) && i % 13 === 0) out.push("Fezz");
+        if (rules.includes(5) && i % 5 === 0) out.push("Buzz");
+        if (rules.includes(7) && i % 7 === 0) out.push("Bang");
+        if (rules.includes(11) && i % 11 === 0) {
+            if (rules.includes(13) && i % 13 === 0) out = ["Fezz", "Bong"];
             else out = ["Bong"];
         }
-        if (i % 17 === 0) out.reverse();
+        if (rules.includes(17) && i % 17 === 0) out.reverse();
 
         console.log(out.length === 0 ? i : out.join(''));
     }
